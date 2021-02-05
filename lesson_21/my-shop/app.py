14500817a8defe9fa0c24faaedc683d4e4233c97
Aleksan <Dev.aleksan@gmail.com>
2021-02-05 23:30:30 +0300

    feat(lesson_21): add start lesson

---
+++ lesson_21/my-shop/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+from views import product_app
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.register_blueprint(product_app, url_prefix="/products")
+db = SQLAlchemy(app)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    name = "World"
+    if request.method == "POST":
+        name = request.form.get('name', 'World')
+    return render_template("index.html", name=name)
+
+
+@app.route("/hello/")
+@app.route("/hello/<string:name>/")
+def hello(name=None):
+    if name is None:
+        name = "World"
+    return f"<h1>Hello {name}!</h1>"
+
+
+@app.route('/post/<int:post_id>/')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post int %d' % post_id
