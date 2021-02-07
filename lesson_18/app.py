2ae3417311889934d17f7962089e6c39f1353d79
Aleksan <Dev.aleksan@gmail.com>
2021-02-07 16:44:01 +0300

    fix: fix style lesson

--- lesson_18/app.py
+++ lesson_18/app.py
@@ -13,24 +13,3 @@ def index():
     return "<h1>Hello world!</h1>"
 
 
-# app.add_url_rule("/", "index", index)
-# app.add_url_rule("/", view_func=index)
-
-@app.route("/hello/")
-@app.route("/hello/<string:name>/")
-def hello(name=None):
-    if name is None:
-        name = "World"
-    return f"<h1>Hello {name}!</h1>"
-
-
-# @app.route('/post/<string:post_id>/')
-# def alternative_show_post(post_id):
-#     post_id > 0
-#     return 'Post (str) %r' % post_id
-
-
-@app.route('/post/<int:post_id>/')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post int %d' % post_id
